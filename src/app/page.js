360509c6f4d309de13a7ef0f31e948956988b161
Auriel James <aurieljames11@gmail.com>
2024-05-29 23:18:19 +0800

    Added Aside

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Header from "@/components/Header";
+import Body from "@/components/Body";
+
 
 export default function Home() {
   return (
     <main className="w-screen">
       <Header />
+      <Body />
     </main>
   );
 }
